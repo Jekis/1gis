812fbc1003f78bf5a1fefce3115f2496dd2a3dd5
Eugene Eroshkin <jekis01@gmail.com>
2015-12-19 14:51:49 +0600

    Response with json on all requests.

--- web/index.php
+++ web/index.php
@@ -1,6 +1,28 @@
 <?php
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 require_once __DIR__.'/../bootstrap.php';
+
+
+// Response with json on all errors.
+$app->error(function (\Exception $e, $code) {
+    $error = array(
+        'message' => $e->getMessage(),
+    );
+
+    // Dirty check, but it's ok for this purpose.
+    $isHttpErrorCode = preg_match('~^[4|5]\d\d$~', $code);
+
+    if ($isHttpErrorCode) {
+        $status = $code;
+    } else {
+        $status = 400;
+        $error['code'] = $code;
+    }
+
+    return new JsonResponse(array('error' => $error), $status);
+});
 
 
 $app->run();
