894237fa6a7fd02806854e573244de3c55acf22e
Eugene Eroshkin <jekis01@gmail.com>
2015-12-19 17:33:02 +0600

    Get buildings from DB .

--- src/App/Controller/BuildingController.php
+++ src/App/Controller/BuildingController.php
@@ -3,19 +3,36 @@
 namespace Gis1\App\Controller;
 
 
+use Doctrine\MongoDB\Cursor;
+use Doctrine\MongoDB\Query\Builder;
+use Silex\Application;
+
 class BuildingController
 {
     use ApiControllerTrait;
 
+    /**
+     * @var Application
+     */
+    protected $app;
+
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
     public function getListAction()
     {
-        $data = array(
-            array(
-                'location' => 'Bluher 32, Novosibirsk, Russia',
-                'x' => 1,
-                'y' => 2,
-            ),
-        );
+        /** @var Builder $qb */
+        $qb = $this->app['mongodb']
+            ->selectCollection('1gis', 'buildings')
+            ->createQueryBuilder()
+        ;
+
+        /** @var Cursor $cursor */
+        $cursor = $qb->find()->limit(50)->getQuery()->execute();
+        $data = $cursor->toArray();
 
         return $this->apiResponse($data);
     }
